5a7d2545f2401d75d98b602dc52d03fcadef3e63
KiriZhu <kiribaty88@gmail.com>
2020-12-12 15:17:21 +0100

    set up redux store and user actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInPage from './pages/signin/sign-in.component';
-import { Switch, Route } from 'react-router-dom';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(null);
