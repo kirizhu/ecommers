d3c9f9dd1bdb7bddd5325b2dc42747adf728a753
KiriZhu <kiribaty88@gmail.com>
2020-12-14 18:52:32 +0100

    cart icon and cart dropdown created, not finished

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import './header.styles.scss';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { auth } from '../../firebase/firebase.utils';
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => {
   return (
@@ -29,7 +32,9 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      <CartDropdown />
     </div>
   );
 };
