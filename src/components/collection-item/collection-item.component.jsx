fccda6bfa50bb92bb52f3c4b2040b84def699593
KiriZhu <kiribaty88@gmail.com>
2020-12-15 13:57:09 +0100

    added prop to custom button, conditionally render cartdropdown

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <CustomButton inverted>ADD TO CART</CustomButton>
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
