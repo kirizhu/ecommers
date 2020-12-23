daba983f3a387e37cd8550a147010294194aa62f
KiriZhu <kiribaty88@gmail.com>
2020-12-23 23:53:24 +0100

    added HOC withspinner to render while fetching data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import { connect } from 'react-redux';
@@ -8,13 +8,20 @@ import {
   convertCollectionsSnapshotToMap,
 } from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.componet';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 const ShopPage = ({ match, updateCollections }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const collectionRef = firestore.collection('collections');
-    const unSubscribe = collectionRef.onSnapshot(async (snapshot) => {
-      const collectionsMap = await convertCollectionsSnapshotToMap(snapshot);
+    const unSubscribe = collectionRef.onSnapshot((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      setIsLoading(false);
     });
     return () => {
       unSubscribe();
@@ -23,8 +30,19 @@ const ShopPage = ({ match, updateCollections }) => {
 
   return (
     <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      <Route
+        exact
+        path={`${match.path}`}
+        render={(props) => (
+          <CollectionsOverviewWithSpinner isLoading={isLoading} {...props} />
+        )}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        render={(props) => (
+          <CollectionPageWithSpinner isLoading={isLoading} {...props} />
+        )}
+      />
     </div>
   );
 };
