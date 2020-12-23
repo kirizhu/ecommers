e4377d161889ae9d40855fbd55bb7d0b182c4d9e
KiriZhu <kiribaty88@gmail.com>
2020-12-23 23:16:21 +0100

    moved shopdata into firebase and connected firebase shopdata to our redux by creating actions and types and adding the case in our reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { connect } from 'react-redux';
 import CollectionPage from '../collection/collection.component';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-const ShopPage = ({ match }) => {
+const ShopPage = ({ match, updateCollections }) => {
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections');
+    const unSubscribe = collectionRef.onSnapshot(async (snapshot) => {
+      const collectionsMap = await convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
@@ -12,4 +29,9 @@ const ShopPage = ({ match }) => {
   );
 };
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
