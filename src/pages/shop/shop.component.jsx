50003f2d99e5ce383f3877a1c3186df784e62eb5
KiriZhu <kiribaty88@gmail.com>
2020-12-24 01:51:45 +0100

    implemented redux thunk and replaced our shop action with our fetch actions and dispatch them all by calling fetchcollectionstartasync

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CollectionPage from '../collection/collection.component';
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selector';
 import WithSpinner from '../../components/with-spinner/with-spinner.componet';
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
-const ShopPage = ({ match, updateCollections }) => {
-  const [isLoading, setIsLoading] = useState(true);
-
+const ShopPage = ({
+  match,
+  isCollectionFetching,
+  fetchCollectionStartAsync,
+}) => {
   useEffect(() => {
-    const collectionRef = firestore.collection('collections');
-
-    // collectionRef.get().then((snapshot) => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //   updateCollections(collectionsMap);
-    //   setIsLoading(false);
-    // });
-    const unSubscribe = collectionRef.onSnapshot((snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      setIsLoading(false);
-    });
+    fetchCollectionStartAsync();
     return () => {
-      unSubscribe();
+      fetchCollectionStartAsync();
     };
   }, []);
 
@@ -40,22 +29,29 @@ const ShopPage = ({ match, updateCollections }) => {
         exact
         path={`${match.path}`}
         render={(props) => (
-          <CollectionsOverviewWithSpinner isLoading={isLoading} {...props} />
+          <CollectionsOverviewWithSpinner
+            isLoading={isCollectionFetching}
+            {...props}
+          />
         )}
       />
       <Route
         path={`${match.path}/:collectionId`}
         render={(props) => (
-          <CollectionPageWithSpinner isLoading={isLoading} {...props} />
+          <CollectionPageWithSpinner
+            isLoading={isCollectionFetching}
+            {...props}
+          />
         )}
       />
     </div>
   );
 };
-
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
+});
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+  fetchCollectionStartAsync: () => dispatch(fetchCollectionStartAsync()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
