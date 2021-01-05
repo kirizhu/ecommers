b2d36847439c25f6d50be4679bd325324e71ca33
KiriZhu <kiribaty88@gmail.com>
2021-01-05 22:53:45 +0100

    stripe payment backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51I15rtIFYynEKL1FCzOzo1sAXEycgqLbdWC6xwbiwmc0IDDlxFOqZQdZ4IS3K951Z5WWHw91BZO9vF020Qc47qzE00VNt5svWH';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment was succesful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please be sure you use the provided credit card'
+        );
+      });
   };
   return (
     <StripeCheckout
