5c2b3c99e51fae85150a25371e7965479ca87eb0
KiriZhu <kiribaty88@gmail.com>
2021-01-19 13:36:46 +0100

    shop and collection refactored to use styled components and to be responsive

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import CollectionItem from '../collection-item/collection-item.component';
-import './collection-preview.styles.scss';
 import { useHistory, useRouteMatch } from 'react-router-dom';
+import CollectionItem from '../collection-item/collection-item.component';
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer,
+} from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items, routeName }) => {
   const history = useHistory();
   const match = useRouteMatch();
   return (
-    <div className='collection-preview'>
-      <h1
-        className='title'
-        onClick={() => history.push(`${match.url}/${routeName}`)}
-      >
+    // <div className='collection-preview'>
+    //   <h1
+    //     className='title'
+    //     onClick={() => history.push(`${match.url}/${routeName}`)}
+    //   >
+    //     {title.toUpperCase()}
+    //   </h1>
+    //   <div className='preview'>
+    //     {items
+    //       .filter((item, i) => i < 4)
+    //       .map((item) => (
+    //         <CollectionItem key={item.id} item={item} />
+    //       ))}
+    //   </div>
+    // </div>
+    <CollectionPreviewContainer>
+      <TitleContainer onClick={() => history.push(`${match.url}/${routeName}`)}>
         {title.toUpperCase()}
-      </h1>
-      <div className='preview'>
+      </TitleContainer>
+      <PreviewContainer>
         {items
-          .filter((item, i) => i < 4)
+          .filter((item, idx) => idx < 4)
           .map((item) => (
             <CollectionItem key={item.id} item={item} />
           ))}
-      </div>
-    </div>
+      </PreviewContainer>
+    </CollectionPreviewContainer>
   );
 };
 
